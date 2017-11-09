3d489c49e42fbc8efc7c6d0882ea87747a33ea5f
nickverstocken <verstockennick@gmail.com>
2017-11-09 16:22:17 +0100

    likes complete

--- src/app/tripcard/tripcard.component.ts
+++ src/app/tripcard/tripcard.component.ts
@@ -1,6 +1,7 @@
 import { Component,Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import {User} from '../Models/User';
+import {TravlrApiService} from '../services/travlr-api.service';
 declare var $: any;
 declare var Swiper: any;
 @Component({
@@ -13,9 +14,12 @@ export class TripcardComponent implements OnInit {
     @Input() currentUser: User;
     @Output() editStop: EventEmitter<any> = new EventEmitter();
     liked: Boolean = false;
-  constructor() { }
+    likes: any[];
+  constructor(private travelrApi: TravlrApiService) { }
 
   ngOnInit() {
+      this.likes = this.stop.likes.data;
+      this.isLiked();
       var mySwiper = new Swiper ('.swiper-container', {
           slidesPerView: 1,
           keyboard: {
@@ -39,13 +43,26 @@ export class TripcardComponent implements OnInit {
       this.editStop.emit(stop);
     }
     isLiked(){
-        let exists = this.stop.likes.data.some(usr => usr.id == this.currentUser.id);
+        let exists = this.likes.some(usr => usr.id == this.currentUser.id);
         if(exists){
             this.liked = true;
         }
-        return this.liked;
     }
-    processLikes(){
-
+    likeStop(){
+        this.travelrApi.likeStop(this.stop).subscribe(
+            result => {
+                console.log(result);
+                if(this.liked){
+                    this.liked = false;
+                    this.stop.like_count -=1;
+                    this.likes = this.likes.filter(usr => usr.id !== this.currentUser.id);
+                }else{
+                    this.liked = true;
+                    this.stop.like_count +=1;
+                    this.likes.unshift(this.currentUser);
+                    this.stop.likes.data = this.likes;
+                }
+            }
+        )
     }
 }
